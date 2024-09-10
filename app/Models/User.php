c152f0b19c26a0c88d842029f391f1965e614a53
Nicolas Giraud <contact@ngiraud.me>
2024-09-10 17:11:48 +0200

    Modelistation des users

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -20,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'avatar',
     ];
 
     /**
@@ -43,5 +45,12 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected function username(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => 'user'.str_pad((string) $this->id, 4, "0", STR_PAD_LEFT),
+        );
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'avatar' => null,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
@@ -39,6 +40,13 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function withAvatar(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'avatar' => 'https://avatar.iran.liara.run/public/'.(fake()->boolean() ? 'girl' : 'boy').'?name='.$attributes['name'],
         ]);
     }
 }
